25a7d92cbe333c52997afb362ccbd756b3086f46
Juan Carlos Vilcherrez <jvilcherrez@buenosaires.gob.ar>
2025-08-26 14:19:31 -0300

    💄  ui: correccion de estilos /nuevo-sitio

--- src/app/nuevo-sitio/page.jsx
+++ src/app/nuevo-sitio/page.jsx
@@ -4,8 +4,11 @@ import { GoogleMaps } from "@/features/nuevo-sitio/components/GoogleMaps"
 
 const page = () => {
     return (
-        <div className="padding-sides pt-5 pb-5">
+        <div className="padding-sides pt-5 pb-5"
+            style={{ backgroundColor: 'var(--bg-neutral)' }}
+        >
             <h2>Nuevo sitio</h2>
+            <p className=" mb-5">Empezá a crear tu sitio, los usuarios podrán descubrirlo en la búsqueda de GoogleMaps</p>
             <div className="d-flex justify-content-between col-12">
                 <Form />
                 <GoogleMaps />
--- src/features/nuevo-sitio/components/Form.jsx
+++ src/features/nuevo-sitio/components/Form.jsx
@@ -1,141 +1,135 @@
 export const Form = () => {
     return (
         <>
-            <div className="col-6 responsive-scroll"
+            <div className="pt-3 pb-4"
                 style={{
                     width: '35rem',
-                    height: '30rem',
-                    overflowY: 'auto',
                     borderRadius: '1rem',
-                    padding: '3rem',
-                    backgroundColor: '#F3F6F9 !important'
+                    backgroundColor: 'white',
+                    overflowY: 'auto',
                 }}
             >
-                <div className="vertical-example">
-                    <h3>Ubicación</h3>
-                    <div className='search-container mb-3'>
-                        <input type="search" className="form-control input-search input-search-with-button" id="search-btn" placeholder="Buscar..." name="search" aria-label="Campo de búsqueda" />
-                        <button className="reset" type="reset" aria-label="Borrar"></button>
-                        <button className="button-search" type="submit" aria-label="Buscar"></button>
+                <div className="vertical-example col-12 p-4 responsive-scroll"
+                    style={{ overflowY: 'auto', height: '35rem' }}
+                >
+                    <div className="col-12">
+                        <h3>Ubicación</h3>
+                        <div className='search-container mb-3'>
+                            <input type="search" className="form-control input-search input-search-with-button" id="search-btn" placeholder="Buscar..." name="search" aria-label="Campo de búsqueda" />
+                            <button className="reset" type="reset" aria-label="Borrar"></button>
+                            <button className="button-search" type="submit" aria-label="Buscar"></button>
+                        </div>
+                        <div className="form-label-container">
+                            <label htmlFor="input-text" className="form-label">
+                                Localidad
+                            </label>
+                        </div>
+                        <input
+                            type="text"
+                            className="form-control mb-3"
+                            id="input-text"
+                            name="campoTexto"
+                            aria-describedby="Texto"
+                            placeholder=""
+                            disabled
+                        />
+                        <div className="form-label-container">
+                            <label htmlFor="input-text" className="form-label">
+                                Provincia
+                            </label>
+                        </div>
+                        <input
+                            type="text"
+                            className="form-control mb-3"
+                            id="input-text"
+                            name="campoTexto"
+                            aria-describedby="Texto"
+                            placeholder=""
+                            disabled
+                        />
+                        <div className="form-label-container">
+                            <label htmlFor="input-text" className="form-label">
+                                Dirección
+                            </label>
+                        </div>
+                        <input
+                            type="text"
+                            className="form-control mb-3"
+                            id="input-text"
+                            name="campoTexto"
+                            aria-describedby="Texto"
+                            placeholder=""
+                            disabled
+                        />
+                        <div className="form-label-container">
+                            <label htmlFor="input-text" className="form-label">
+                                Dirección Adicional
+                            </label>
+                        </div>
+                        <input
+                            type="text"
+                            className="form-control mb-3"
+                            id="input-text"
+                            name="campoTexto"
+                            aria-describedby="Texto"
+                            placeholder=""
+                        />
+                        <h3 className="mt-4">Perfil del sitio</h3>
+                        <div className="form-label-container">
+                            <label htmlFor="input-text" className="form-label">
+                                Nombre
+                            </label>
+                        </div>
+                        <input
+                            type="text"
+                            className="form-control mb-3"
+                            id="input-text"
+                            name="campoTexto"
+                            aria-describedby="Texto"
+                            placeholder=""
+                        />
+                        <div className="form-label-container">
+                            <label htmlFor="input-select" className="form-label">
+                                Categoría
+                            </label>
+                        </div>
+                        <select className="form-control form-select mb-3" aria-label="Ejemplo de selección por defecto">
+                            <option value="0">Seleccionar</option>
+                            <option value="1">Galería de Arte</option>
+                            <option value="2">Contable</option>
+                            <option value="3">Clínica</option>
+                            <option value="3">Escuela</option>
+                            <option value="3">Centro Cultural</option>
+                        </select>
+                        <div className="form-label-container">
+                            <label htmlFor="input-select" className="form-label">
+                                Grupo
+                            </label>
+                        </div>
+                        <select className="form-control form-select mb-3" aria-label="Ejemplo de selección por defecto">
+                            <option value="0">Seleccionar</option>
+                            <option value="1">Cultura</option>
+                            <option value="2">Educación</option>
+                            <option value="3">Clínica</option>
+                            <option value="3">Salud</option>
+                            <option value="3">Transporte</option>
+                        </select>
+                        <div className="form-label-container">
+                            <label htmlFor="input-text" className="form-label">
+                                Organismo
+                            </label>
+                        </div>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="input-text"
+                            name="campoTexto"
+                            aria-describedby="Texto"
+                            placeholder=""
+                            value='DGSACIU'
+                            disabled
+                        />
                     </div>
-                    <div className="form-label-container">
-                        <label htmlFor="input-text" className="form-label">
-                            Localidad
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        className="form-control mb-3"
-                        id="input-text"
-                        name="campoTexto"
-                        aria-describedby="Texto"
-                        placeholder="Ingrese Textos"
-                    />
-                    <div className="form-label-container">
-                        <label htmlFor="input-text" className="form-label">
-                            Provincia
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        className="form-control mb-3"
-                        id="input-text"
-                        name="campoTexto"
-                        aria-describedby="Texto"
-                        placeholder="Ingrese Textos"
-                    />
-                    <div className="form-label-container">
-                        <label htmlFor="input-text" className="form-label">
-                            Dirección
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        className="form-control mb-3"
-                        id="input-text"
-                        name="campoTexto"
-                        aria-describedby="Texto"
-                        placeholder="Ingrese Textos"
-                    />
-                    <div className="form-label-container">
-                        <label htmlFor="input-text" className="form-label">
-                            Dirección Adicional
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        className="form-control mb-3"
-                        id="input-text"
-                        name="campoTexto"
-                        aria-describedby="Texto"
-                        placeholder="Ingrese Textos"
-                    />
-                    <h3 className="mt-4">Perfil del sitio</h3>
-                    <div className="form-label-container">
-                        <label htmlFor="input-text" className="form-label">
-                            Nombre
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        className="form-control mb-3"
-                        id="input-text"
-                        name="campoTexto"
-                        aria-describedby="Texto"
-                        placeholder="Ingrese Textos"
-                    />
-                    <div className="form-label-container">
-                        <label htmlFor="input-text" className="form-label">
-                            Dirección Adicional
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        className="form-control mb-3"
-                        id="input-text"
-                        name="campoTexto"
-                        aria-describedby="Texto"
-                        placeholder="Ingrese Textos"
-                    />
-                    <div className="form-label-container">
-                        <label htmlFor="input-text" className="form-label">
-                            Dirección Adicional
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        className="form-control mb-3"
-                        id="input-text"
-                        name="campoTexto"
-                        aria-describedby="Texto"
-                        placeholder="Ingrese Textos"
-                    />
-                    <div className="form-label-container">
-                        <label htmlFor="input-text" className="form-label">
-                            Dirección Adicional
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        className="form-control mb-3"
-                        id="input-text"
-                        name="campoTexto"
-                        aria-describedby="Texto"
-                        placeholder="Ingrese Textos"
-                    />
-                    <div className="form-label-container">
-                        <label htmlFor="input-text" className="form-label">
-                            Dirección Adicional
-                        </label>
-                    </div>
-                    <input
-                        type="text"
-                        className="form-control pb-3"
-                        id="input-text"
-                        name="campoTexto"
-                        aria-describedby="Texto"
-                        placeholder="Ingrese Textos"
-                    />
                 </div>
             </div>
         </>
